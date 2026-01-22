aa4f6c322e2f3235271f55f5abf48347cc8ab93e
offlinesmartfridge <jurajbalog9991@gmail.com>
2026-01-22 05:53:25 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/Hoses-Roses/',
   plugins: [
     vue(),
     vueDevTools(),
